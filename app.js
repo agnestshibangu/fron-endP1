d52f66184a1bdb30ed2ccb616d08e7db339325b1
Agnes <agnes.tshibangugrillon@gmail.com>
2021-05-24 00:42:31 +0200

    animation card + search

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+
 window.addEventListener("scroll", function() {
     const mainNav = document.getElementById("navbar");
 
     if(window.pageYOffset > 80) {
         mainNav.classList.add("bg-pink");
-        console.log('hi');
+    } else if(this.window.pageYOffset < 80) {
+        mainNav.classList.remove("bg-pink");
     }
 }); 
