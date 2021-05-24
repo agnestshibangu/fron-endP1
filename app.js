073c377234222382548e0b6186922197c7a0b11e
Agnes <agnes.tshibangugrillon@gmail.com>
2021-05-24 23:28:02 +0200

    items pages

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ window.addEventListener("scroll", function() {
     const mainNav = document.getElementById("navbar");
     const bigImg = document.getElementById("container_big-img");
 
-    if(window.pageYOffset > 30) {
+
+    if(window.pageYOffset > 10) {
         mainNav.classList.add("bg-pink");
-        mainNav.style.height = "100px";
+        mainNav.style.height = "75px";
         bigImg.style.height = "0px";
+        
         console.log('hi');
-    } else if(this.window.pageYOffset < 10) {
+    } else if(this.window.pageYOffset < 5) {
         mainNav.classList.remove("bg-pink");
         mainNav.style.height = "100px";
         bigImg.style.height = "100vh";
