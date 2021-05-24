89157ea02d300dc8d143b3c0ff94e6491cfbcc95
Agnes <agnes.tshibangugrillon@gmail.com>
2021-05-24 20:29:25 +0200

    scroll effect javascript

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-
 window.addEventListener("scroll", function() {
     const mainNav = document.getElementById("navbar");
+    const bigImg = document.getElementById("container_big-img");
 
-    if(window.pageYOffset > 80) {
+    if(window.pageYOffset > 30) {
         mainNav.classList.add("bg-pink");
-    } else if(this.window.pageYOffset < 80) {
+        mainNav.style.height = "100px";
+        bigImg.style.height = "0px";
+        console.log('hi');
+    } else if(this.window.pageYOffset < 10) {
         mainNav.classList.remove("bg-pink");
+        mainNav.style.height = "100px";
+        bigImg.style.height = "100vh";
+
     }
 }); 
+
+/////////////////////////
+
+function toggleSidebar(ref){
+    document.getElementById("sidebar").classList.toggle('active');
+}
